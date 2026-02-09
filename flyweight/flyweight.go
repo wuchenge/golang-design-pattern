2f549593f6b540167fd8ee6cf391a2aedd7689e3
wuchen <wuchen@topvan.org>
2026-02-09 16:24:08 +0800

    feat: add flyweight pattern

---
+++ flyweight/flyweight.go
@@ -0,0 +1,59 @@
+package flyweight
+
+import "fmt"
+
+type ImageFlyweightFactory struct {
+	maps map[string]*ImageFlyweight
+}
+
+var imageFactory *ImageFlyweightFactory
+
+func GetImageFlyweightFactory() *ImageFlyweightFactory {
+	if imageFactory == nil {
+		imageFactory = &ImageFlyweightFactory{
+			maps: make(map[string]*ImageFlyweight),
+		}
+	}
+
+	return imageFactory
+}
+
+func (f *ImageFlyweightFactory) get(filename string) *ImageFlyweight {
+	image, ok := f.maps[filename]
+	if !ok {
+		image = NewImageFlyweight(filename)
+		f.maps[filename] = image
+	}
+
+	return image
+}
+
+type ImageFlyweight struct {
+	data string
+}
+
+func NewImageFlyweight(filename string) *ImageFlyweight {
+	data := fmt.Sprintf("image data %s", filename)
+	return &ImageFlyweight{
+		data: data,
+	}
+}
+
+func (i *ImageFlyweight) Data() string {
+	return i.data
+}
+
+type ImageViewer struct {
+	*ImageFlyweight
+}
+
+func NewImageViewer(filename string) *ImageViewer {
+	image := GetImageFlyweightFactory().get(filename)
+	return &ImageViewer{
+		ImageFlyweight: image,
+	}
+}
+
+func (i *ImageViewer) Display() {
+	fmt.Printf("Display: %s\n", i.Data())
+}
---
+++ flyweight/flyweight_test.go
@@ -0,0 +1,21 @@
+package flyweight
+
+import "testing"
+
+func Example() {
+	viewer := NewImageViewer("image1.png")
+	viewer.Display()
+	// Output:
+	// Display: image data image1.png
+}
+
+func TestFlyweight(t *testing.T) {
+	viewer1 := NewImageViewer("image1.png")
+	viewer2 := NewImageViewer("image1.png")
+
+	if viewer1.ImageFlyweight != viewer2.ImageFlyweight {
+		t.Fail()
+	}
+
+	viewer1.Display()
+}
