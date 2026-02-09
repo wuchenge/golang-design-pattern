0132bd5872cb793ee3e811b670f4c846ee0c9e72
wuchen <wuchen@topvan.org>
2026-02-09 10:07:44 +0800

    feat: addd command pattern

---
+++ command/command_test.go
@@ -0,0 +1,19 @@
+package command
+
+import "testing"
+
+func TestCommand(t *testing.T) {
+	mb := &MotherBoard{}
+	startCommand := NewStartCommand(mb)
+	rebootCommand := NewRebootCommand(mb)
+
+	box1 := NewBox(startCommand, rebootCommand)
+	box1.PressButton1()
+	box1.PressButton2()
+
+	box2 := NewBox(rebootCommand, startCommand)
+	box2.PressButton1()
+	box2.PressButton2()
+
+	t.Logf("testing...")
+}
