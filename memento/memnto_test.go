7c4cc735a1af88419dfe59841c161ac944cb3ed3
wuchen <wuchen@topvan.org>
2026-02-09 15:52:57 +0800

    feat: add memento pattern

---
+++ memento/memnto_test.go
@@ -0,0 +1,20 @@
+package memento
+
+import "testing"
+
+func TestGame(t *testing.T) {
+	game := &Game{
+		hp: 10,
+		mp: 10,
+	}
+
+	game.Status()
+	progress := game.Save()
+	game.Play(-2, -3)
+	game.Status()
+
+	game.Load(progress)
+	game.Status()
+
+	t.Log("test ...")
+}
